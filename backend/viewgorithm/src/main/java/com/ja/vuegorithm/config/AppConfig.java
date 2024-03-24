4ca0dd671b4b1716df8b3a479cf0dbdf416e7d71
Gun-taek <133934005+Gun-taek@users.noreply.github.com>
2024-03-24 18:28:53 +0900

    Dto클래스 생성 및 백엔드 프로젝트 설정

---
+++ backend/viewgorithm/src/main/java/com/ja/vuegorithm/config/AppConfig.java
@@ -0,0 +1,43 @@
+package com.ja.vuegorithm.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// import com.ace.skream.user.commons.interceptor.SessionInterceptor;
+
+@Configuration
+@EnableWebMvc
+@EnableScheduling
+public class AppConfig implements WebMvcConfigurer{
+
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+
+        // 아래 3개 폴더 생성
+        // /static/public/js
+        // /static/public/css
+        // /static/public/image
+
+        
+        registry.addResourceHandler("/public/**")
+            .addResourceLocations("classpath:/static/public/")
+            // .setCachePeriod(60 * 60 * 24 * 365);
+        ;  
+        
+        // 업로드 외부 파일 
+        registry.addResourceHandler("/uploadFiles/**")
+            .addResourceLocations("file://C:/uploadFiles/")
+            // .setCachePeriod(60 * 60 * 24 * 365);
+        ;  
+	}    
+
+}
+
+
+
+
---
+++ backend/viewgorithm/src/main/java/com/ja/vuegorithm/user/UserController.java
@@ -0,0 +1,18 @@
+package com.ja.vuegorithm.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/Vuegorithm/*")
+public class UserController {
+    
+    // 메인페이지
+    @RequestMapping("MainPage")
+    public String MainPage(){
+                return "user/MainPage";
+    }
+
+}
